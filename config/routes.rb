cc88e476b5f8d1a703017bdb77b8f472334f714e
EmilyPlatzer <emilyplatzer@gmail.com>
2015-04-22 10:03:05 -0600

    Hand-rolled routes for resource plants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :plants
+  # resources :plants
+  get '/plants' => 'plants#index', as: 'plants'
+  get '/plants/new' => 'plants#new', as: 'new_plant'
+  post '/plants' => 'plants#create'
+  get '/plants/:id' => 'plants#show', as: 'plant'
+  get '/plants/:id/edit' => 'plants#edit', as: 'edit_plant'
+  patch '/plants/:id' => 'plants#update'
+  delete 'plants/:id' => 'plants#destroy'
 end
